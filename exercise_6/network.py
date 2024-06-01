71f52614649bbe60f94d413ed9c18eaf43ff98a1
Hajrullahu, Ilir <ilir.hajrullahu@accso.de>
2024-06-01 16:27:12 +0200

    working on linear autoencoder

--- exercise_6/network.py
+++ exercise_6/network.py
@@ -34,34 +34,35 @@ class Autoencoder(nn.Module):
         super(Autoencoder, self).__init__()
         self.encoder = nn.Sequential(
             ########## YOUR CODE HERE #############
-            nn.Linear(28,128),
-            nn.ReLU,
+            nn.Linear(784,128),
+            nn.ReLU(),
             nn.Linear(128,64),
-            nn.ReLU,
+            nn.ReLU(),
             nn.Linear(64,32),
-            nn.ReLU,
+            nn.ReLU(),
             nn.Linear(32,16),
-            nn.ReLU,
+            nn.ReLU(),
             nn.Linear(16,8),
-            nn.ReLU
+            nn.ReLU()
         )
         self.decoder = nn.Sequential(
             ########## YOUR CODE HERE #############
             nn.Linear(8,8),
-            nn.ReLU,
+            nn.ReLU(),
             nn.Linear(8,32),
-            nn.ReLU,
+            nn.ReLU(),
             nn.Linear(32,64),
-            nn.ReLU,
+            nn.ReLU(),
             nn.Linear(64,128),
-            nn.ReLU,
+            nn.ReLU(),
             nn.Linear(128, 784),
             nn.Tanh()
         )
 
     def forward(self, x):
         ########## YOUR CODE HERE #############
-        
+        h = self.encoder(x)
+        x = self.decoder(h)
         return x
 
 
@@ -70,14 +71,31 @@ class Conv_Autoencoder(nn.Module):
         super(Conv_Autoencoder,self).__init__()
         self.encoder = nn.Sequential(
            ########## YOUR CODE HERE #############
+           nn.Conv2d(in_channels=1,out_channels=4,kernel_size=5), # 28x28x1 to 24x24x4
+           nn.ReLU(), # stays 24x24x6
+           nn.Conv2d(in_channels=4,out_channels=8,kernel_size=5), # 24x24x6 to 20x20x8 
+           nn.ReLU(),
+           nn.Flatten(), # 20x20x8 to 3200
+           nn.Linear(3200,10),
+           nn.Softmax()
             )
         
         self.decoder = nn.Sequential(
            ########## YOUR CODE HERE #############
+           nn.Linear(10,400),
+           nn.ReLU(),
+           nn.Linear(400,4000),
+           nn.Unflatten(1,(8,25,20)),
+           nn.ConvTranspose2d(in_channels=10,out_channels=10,kernel_size=5),
+           nn.ReLU(),
+           nn.ConvTranspose2d(in_channels=10,out_channels=1,kernel_size=5),
+           nn.Tanh()
             )
         
     def forward(self, x):
         x = x.view(-1, 1, 28, 28)
         ########## YOUR CODE HERE #############
         x = x.view(-1, 28*28)
+        x = self.encoder(x)
+        x = self.decoder(x)
         return x
